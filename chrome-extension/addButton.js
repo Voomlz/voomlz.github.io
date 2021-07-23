70d14d7b71d645e4d744128872cdf04d508b8bac
voomlz <voomlab@gmail.com>
2021-07-23 14:01:34 +0200

    Adding testing page

--- chrome-extension/addButton.js
+++ chrome-extension/addButton.js
@@ -1,10 +1,9 @@
-let url = "https://voomlz.github.io/threat?id=" + location.origin + location.pathname;
+let url = "https://voomlz.github.io?id=" + location.origin + location.pathname;
 let bigTabContainer = document.getElementById("top-level-view-tabs-container");
 let anchor = document.createElement("a");
 let span = document.createElement("span");
 span.className = "big-tab-text";
 span.textContent = "Threat Graph";
 anchor.href = url;
-//anchor.innerText = 'Go to Google';
 anchor.appendChild(span)
 bigTabContainer.appendChild(anchor)
