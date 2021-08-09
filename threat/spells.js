55c97bf763ca0a66e51af6664c3bd1e94827072b
voomlz <voomlab@gmail.com>
2021-08-09 11:46:14 +0200

    Removing refresh sunder from devastate

--- threat/spells.js
+++ threat/spells.js
@@ -546,7 +546,7 @@ function handler_sunderArmor(threatValue) {
             return;
         }
 
-        if (ev.type === "refreshdebuff" || ev.type === "applydebuff") {
+        if (ev.type === "applydebuff") {
             lastSunderEvent = ev;
         }
     }
