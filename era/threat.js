36df3bbb5f09b155281d25148b062565700405ee
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-01 10:31:14 +1000

    [Era] Better mobile support and graph size
    - Adjusts the default html to work better on mobile (larger font)
    - Sets the graph to be the width of the window size instead of a fixed size
    
    These assumptions/fixes can be changed later but they're better than nothing.

--- era/threat.js
+++ era/threat.js
@@ -8,6 +8,8 @@ let recolorPlot = () => {
 };
 let colorByClass = true;
 
+const SCROLLBAR_WIDTH = 16;
+
 function printError(e) {
     console.log(e);
     alert("Error:\n" + e + "\n\nRefresh the page to start again.");
@@ -560,8 +562,8 @@ class NPC extends Unit {
                 gridcolor: "#666",
                 linecolor: "#999"
             },
-            width: 1920,
-            height: 1080,
+            width: window.innerWidth - SCROLLBAR_WIDTH,
+            height: (window.innerWidth - SCROLLBAR_WIDTH) / (1920/1080),
             hovermode: "closest",
             plot_bgcolor: "#222",
             paper_bgcolor: "#222",
