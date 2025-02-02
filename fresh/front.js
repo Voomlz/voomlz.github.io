137ade3ffa262e0a958b6a326d49ba3958009c43
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-02 23:37:15 +1000

    [Fresh] Fix console error (#11)
    Fixes the console error about `getParameterByName`

--- fresh/front.js
+++ fresh/front.js
@@ -131,3 +131,12 @@ function setParamAndReload() {
 function redirectToThreat() {
     location.href = location.origin + location.pathname + "/threat";
 }
+
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
