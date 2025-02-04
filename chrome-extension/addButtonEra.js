11e318d3c6fd3f057e0f7ea50bd224fafb6b6d7e
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-05 09:26:02 +1100

    Add BETA badge to extension, fix fresh
    - There was an error with the fresh matcher, fixed
    - Added a badge to the button:

--- chrome-extension/addButtonEra.js
+++ chrome-extension/addButtonEra.js
@@ -1,7 +1,6 @@
 let url =
   "https://voomlz.github.io/era/?id=" + location.origin + location.pathname;
 let bigTabContainer = document.getElementById("top-level-view-tabs");
-let anchor = document.createElement("a");
 
 let spanText = document.createElement("span");
 spanText.className = "big-tab-text";
@@ -13,7 +12,9 @@ let spanIcon = document.createElement("span");
 spanIcon.className = "zmdi zmdi-chart";
 spanIcon.class = "big-tab view-type-tab selected";
 
+let anchor = document.createElement("a");
 anchor.href = url;
+anchor.target = "_blank";
 anchor.id = "threat-graph-link";
 anchor.className = "big-tab view-type-tab";
 anchor.appendChild(spanIcon);
--- chrome-extension/addButtonSod.js
+++ chrome-extension/addButtonSod.js
@@ -1,19 +1,29 @@
 let url =
   "https://voomlz.github.io/sod/?id=" + location.origin + location.pathname;
 let bigTabContainer = document.getElementById("top-level-view-tabs");
-let anchor = document.createElement("a");
 
 let spanText = document.createElement("span");
 spanText.className = "big-tab-text";
-spanText.appendChild(document.createElement("br"));
-spanText.innerHTML = "<br> Threat Graph";
+spanText.style = "position: relative";
+spanText.innerHTML = `<span style="
+    position: absolute;
+    color: red;
+    font-weight: bold;
+    transform: rotate(-8deg);
+    left: 0;
+    top: 8px;
+    font-size: 10px;
+">BETA</span>
+<br> Threat Graph`;
 spanText.class = "big-tab view-type-tab selected";
 
 let spanIcon = document.createElement("span");
 spanIcon.className = "zmdi zmdi-chart";
 spanIcon.class = "big-tab view-type-tab selected";
 
+let anchor = document.createElement("a");
 anchor.href = url;
+anchor.target = "_blank";
 anchor.id = "threat-graph-link";
 anchor.className = "big-tab view-type-tab";
 anchor.appendChild(spanIcon);
