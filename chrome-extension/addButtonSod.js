8dcd85c49f94c0048b634d0c2fc2b4dc9cdac601
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-05 05:44:57 +1000

    SoD support (#6)
    * Inital Add SOD files
    
    * Reuse era files (#2)
    
    * Add SoD to Firefox extension (#3)
    
    * Updated spell schools and moved items
    
    * Add SoD button to the Chrome extension (#4)
    
    * update to original url
    
    * restore original formatting for html
    
    * Specify Ferocity is Hunter T1 2pc
    
    * Undo formatting changes in unchanged files
    
    * REvert unchanged files, attempt2
    
    * Remove line endings
    
    * Added some bear form coefficients
    
    * More druid updates
    
    * Cat form = 30% reduction
    
    * Druid cat fixup (.71 tested), and Moonkin implications
    
    * Added some Rogue tank stuff and cleanup constants
    
    * Resto Druid subtlety, and more Rogue tank stuff
    
    * Rogue: Unfair Advantage
    
    * Sort bosses above trash, graph sizing
    
    * Basic Rogue tank support, added Class.Buff to notableBuffs
    
    * dont double count manual Flesh Wound buff
    
    * More Rogue fixes, initialBuffs changes (needs era/threat.js changes)
    
    * [Era] Sort fights, enemies, targets
    
    * Extract out initialBuffs config, allow spellId buff multipliers
    
    - Extract out initialBuffs, so we can add more per-class ones in spells.js - Will be used in SoD, but can work in era if we need
    - Allow a buffMultiplier to apply only to certain spells via a `coef(buffs, spellId)` function.
    
    * Use object spreads instead
    
    * SoD Warrior updates
    
    * Fix up Defiance additive coeff
    
    * [Era] Better mobile support and graph size
    
    - Adjusts the default html to work better on mobile (larger font)
    - Sets the graph to be the width of the window size instead of a fixed size
    
    These assumptions/fixes can be changed later but they're better than nothing.
    
    * Copy mobile fix for SoD
    
    * Add combatant gear checks for threat enchants
    
    * Allow a `combatantImplications` structure to configure gear/talents
    
    * Add Warrior tier set implications (T1 and T2.5 so far)
    
    * Add note about pally
    
    * Restore era handlers for naxx
    
    * Fix accidental overwrite
    
    * Say sod not era in html note
    
    * Update Warrior sets, basic Paladin RF, ImpRF and HoR "tank rune"
    
    * Remove old heroic strike handler
    
    * Paladin comments for clarification
    
    ---------
    
    Co-authored-by: Andrew <andrew.daniels@live.com>
    Co-authored-by: DevilinAus <112529379+DevilinAus@users.noreply.github.com>

---
+++ chrome-extension/addButtonSod.js
@@ -0,0 +1,22 @@
+let url =
+  "https://voomlz.github.io/sod/?id=" + location.origin + location.pathname;
+let bigTabContainer = document.getElementById("top-level-view-tabs");
+let anchor = document.createElement("a");
+
+let spanText = document.createElement("span");
+spanText.className = "big-tab-text";
+spanText.appendChild(document.createElement("br"));
+spanText.innerHTML = "<br> Threat Graph";
+spanText.class = "big-tab view-type-tab selected";
+
+let spanIcon = document.createElement("span");
+spanIcon.className = "zmdi zmdi-chart";
+spanIcon.class = "big-tab view-type-tab selected";
+
+anchor.href = url;
+anchor.id = "threat-graph-link";
+anchor.className = "big-tab view-type-tab";
+anchor.appendChild(spanIcon);
+anchor.appendChild(spanText);
+
+bigTabContainer.appendChild(anchor);
