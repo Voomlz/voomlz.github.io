0913e18bcb4d9b87aaf532329ea0e322dd65acc9
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-11 22:03:55 +1000

    [SoD] Ret Pally and Shaman Tank updates (#15)
    * [SoD] Add Ret Pally Vengeance
    
    This should make ret pallies less threaty in the graph
    
    * Shaman updates

--- sod/shaman.js
+++ sod/shaman.js
@@ -1,18 +1,49 @@
 const shaman = (function () {
   const config = {
+    Mods: {
+      MoltenBlast: 2.0,
+      EarthShock: 2.0,
+      SpiritOfTheAlpha: 1.45,
+
+      /**
+       * While Rockbiter Weapon is active on your main hand weapon and you have a shield equipped,
+       * you deal 65% increased threat
+       */
+      WayOfEarth: 1.65,
+
+      LoyalBeta: 0.7,
+    },
     Buff: {
       TranquilAirTotem: 25909,
+      SpiritOfTheAlpha: 408696,
+      LoyalBeta: 443320,
+    },
+    Spell: {
+      MoltenBlast: 425339,
     },
   };
 
-  const initialBuffs = {};
+  const initialBuffs = {
+    [config.Buff.SpiritOfTheAlpha]: 0,
+    [config.Buff.WayOfEarth]: 0,
+  };
 
   const buffNames = {
     [config.Buff.TranquilAirTotem]: "Tranquil Air Totem",
+    [config.Buff.SpiritOfTheAlpha]: "Spirit of the Alpha",
+    [config.Buff.LoyalBeta]: "Loyal Beta",
+    [config.Buff.WayOfEarth]: "Way of Earth",
   };
 
   const buffMultipliers = {
     [config.Buff.TranquilAirTotem]: getThreatCoefficient(0.8), // Tranquil Air Totem Aura
+    [config.Buff.SpiritOfTheAlpha]: getThreatCoefficient(
+      config.Mods.SpiritOfTheAlpha
+    ),
+    [config.Buff.LoyalBeta]: getThreatCoefficient(config.Mods.LoyalBeta),
+
+    // TODO: only when rockbiter weapon is active on MH
+    // [config.Buff.WayOfEarth]: getThreatCoefficient(config.Mods.WayOfEarth),
   };
 
   const HEALING_SPELLS = {
@@ -48,13 +79,15 @@ const shaman = (function () {
   const fixateBuffs = {};
 
   const spellFunctions = {
-    8042: handler_modDamage(2), // Earth Shock r1
-    8044: handler_modDamage(2), // Earth Shock r2
-    8045: handler_modDamage(2), // Earth Shock r3
-    8046: handler_modDamage(2), // Earth Shock r4
-    10412: handler_modDamage(2), // Earth Shock r5
-    10413: handler_modDamage(2), // Earth Shock r6
-    10414: handler_modDamage(2), // Earth Shock r7
+    8042: handler_modDamage(config.Mods.EarthShock), // Earth Shock r1
+    8044: handler_modDamage(config.Mods.EarthShock), // Earth Shock r2
+    8045: handler_modDamage(config.Mods.EarthShock), // Earth Shock r3
+    8046: handler_modDamage(config.Mods.EarthShock), // Earth Shock r4
+    10412: handler_modDamage(config.Mods.EarthShock), // Earth Shock r5
+    10413: handler_modDamage(config.Mods.EarthShock), // Earth Shock r6
+    10414: handler_modDamage(config.Mods.EarthShock), // Earth Shock r7
+
+    [config.Spell.MoltenBlast]: handler_modDamage(config.Mods.MoltenBlast),
   };
 
   const combatantImplications = (unit, buffs, talents) => {};
@@ -65,7 +98,9 @@ const shaman = (function () {
 
   const invulnerabilityBuffs = {};
 
-  const auraImplications = {};
+  const auraImplications = {
+    [config.Spell.MoltenBlast]: config.Buff.SpiritOfTheAlpha,
+  };
 
   return {
     buffMultipliers,
