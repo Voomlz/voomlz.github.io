8733a04d98a7718a32f42beab0c3190e57fb4471
Tim Stirrat <tim.stirrat@gmail.com>
2025-02-19 17:24:36 +1000

    [Era] Extract classes into modules

---
+++ era/class/hunter.js
@@ -0,0 +1,54 @@
+import {
+  getThreatCoefficient,
+  handler_castCanMiss,
+  handler_threatOnHit,
+  handler_vanish,
+} from "../base.js";
+
+export const config = {
+  Buff: {},
+  Mods: {},
+  Spell: {
+    FeignDeath: 5384,
+    DistractingShotR1: 20736,
+    DistractingShotR2: 14274,
+    DistractingShotR3: 15629,
+    DistractingShotR4: 15630,
+    DistractingShotR5: 15631,
+    DistractingShotR6: 15632,
+    DisengageR1: 781,
+    DisengageR2: 14272,
+    DisengageR3: 14273,
+  },
+};
+
+export const initialBuffs = {};
+
+export const buffNames = {};
+
+export const auraImplications = {};
+
+export const buffMultipliers = {};
+
+export const talents = {};
+
+export const fixateBuffs = {};
+
+export const spellFunctions = {
+  [config.Spell.FeignDeath]: handler_vanish, // Feign Death
+  [config.Spell.DistractingShotR1]: handler_threatOnHit(110), // Distracting Shot r1
+  [config.Spell.DistractingShotR2]: handler_threatOnHit(160), // Distracting Shot r2
+  [config.Spell.DistractingShotR3]: handler_threatOnHit(250), // Distracting Shot r3
+  [config.Spell.DistractingShotR4]: handler_threatOnHit(350), // Distracting Shot r4
+  [config.Spell.DistractingShotR5]: handler_threatOnHit(465), // Distracting Shot r5
+  [config.Spell.DistractingShotR6]: handler_threatOnHit(600), // Distracting Shot r6
+  [config.Spell.DisengageR1]: handler_castCanMiss(-140), // Disengage Rank 1
+  [config.Spell.DisengageR2]: handler_castCanMiss(-280), // Disengage Rank 2
+  [config.Spell.DisengageR3]: handler_castCanMiss(-405), // Disengage Rank 3
+};
+
+export const combatantImplications = (unit, buffs, talents) => {};
+
+export const notableBuffs = {
+  ...Object.values(config.Buff),
+};
---
+++ era/class/rogue.js
@@ -0,0 +1,44 @@
+import {
+  getThreatCoefficient,
+  handler_castCanMissNoCoefficient,
+  handler_vanish,
+} from "../base.js";
+
+export const config = {
+  Mods: {
+    Base: 0.71,
+  },
+  Spell: {},
+  Buff: {},
+};
+
+export const initialBuffs = {};
+
+export const buffNames = {};
+
+export const buffMultipliers = {};
+
+export const talents = {};
+
+export const fixateBuffs = {};
+
+export const auraImplications = {};
+
+export const spellFunctions = {
+  // Rogue
+  1856: handler_vanish,
+  1857: handler_vanish, // Vanish
+  1966: handler_castCanMissNoCoefficient(-150), // Feint r1
+  6768: handler_castCanMissNoCoefficient(-240), // Feint r2
+  8637: handler_castCanMissNoCoefficient(-390), // Feint r3
+  11303: handler_castCanMissNoCoefficient(-600), // Feint r4
+  25302: handler_castCanMissNoCoefficient(-800), // Feint r5
+};
+
+export const notableBuffs = {
+  ...Object.values(config.Buff),
+};
+
+export const combatantImplications = (unit, buffs, talents) => {};
+
+export const baseThreatCoefficient = getThreatCoefficient(config.Mods.Base);
