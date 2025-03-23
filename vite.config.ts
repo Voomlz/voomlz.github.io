b2fe3f72476ceab7e5610a077a237dcf9e23b7e3
Tim Stirrat <tim.stirrat@gmail.com>
2025-03-23 10:15:30 +1000

    [manual] Add vitest and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+  },
+  test: {
+    exclude: ["**/*.spec.ts"],
+    mockReset: true,
+    clearMocks: true,
+    restoreMocks: true,
+  },
+});
